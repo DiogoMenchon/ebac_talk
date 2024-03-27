bc5afe91ec11ca95788d9c238f4598cd897a7249
Diogo Peres <plaer654@gmail.com>
2024-03-27 10:03:20 -0300

    ebac-talk

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,10 +14,10 @@ const contaAsHoras = setInterval(function () {
     const minutoEmMs = 1000 * 60;
 
     const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
-    const horasAteOEvento = Math.floor(distanciaAteOEvento % (diaEmMs)) / (horaEmMs);
-    const minutosAteOEvento = Math.floor(distanciaAteOEvento % (horaEmMs)) / minutoEmMs;
-    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+    const horasAteOEvento = Math.floor(distanciaAteOEvento % (diaEmMs) / (horaEmMs));
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento % (horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs / 1000));
     console.log(diasAteOEvento);
 
     document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
-}, 1000)
+}, 1000);
